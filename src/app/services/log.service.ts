19d8ee602733bcbdccab1d867aa2363849ff4ed9
Ryan Raymond <ryan@Ryans-iMac.local>
2020-05-04 11:29:18 -0400

    added delete functionality

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -55,4 +55,11 @@ export class LogService {
     });
     this.logs.unshift(log);
   }
+  deleteLog(log: Log) {
+    this.logs.forEach((cur, index) => {
+      if (log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
+  }
 }
